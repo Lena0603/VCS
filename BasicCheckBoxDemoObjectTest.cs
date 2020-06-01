95e630bd753fbfe4d0ee9c656cda7a907bc4e66f
Lena0603 <66062541+Lena0603@users.noreply.github.com>
2020-06-01 17:59:05 +0300

    Add files via upload

---
+++ BasicCheckBoxDemoObjectTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using VcsWebdriver.Pages;
+
+
+
+
+namespace VcsWebdriver.Tests
+{
+    public class BasicCheckBoxDemoTest
+    {
+        private static BasicCheckBoxDemoPage _seleniumBasicCheckBoxPage;
+
+        [OneTimeSetUp]
+        public static void SetUpChrome()
+        {
+            var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            _seleniumBasicCheckBoxPage = new BasicCheckBoxDemoPage(driver);
+
+        }
+
+        [Test]
+        public static void PazymimeClickOnThisCheckBox()
+        {
+            string tekstas = "Success - Check box is checked";
+            _seleniumBasicCheckBoxPage.ClickCheckBox();
+            _seleniumBasicCheckBoxPage.SuccesTekstoRodymas(tekstas);
+        }
+
+        [Test]
+        public static void PazymimeMultipleCheckBox()
+        {
+            _seleniumBasicCheckBoxPage.ClickAllOptions();
+            _seleniumBasicCheckBoxPage.MygtukoTekstoRezultatas("Uncheck All");
+
+        }
+
+
+        [Test]
+        public static void PaspaudziameUncheckAll()
+        {
+            _seleniumBasicCheckBoxPage.UncheckMygtukoPaspaudimas();
+            _seleniumBasicCheckBoxPage.UncheckMygtukoPatikra();
+        }
+
+
+
+
+        [OneTimeTearDown]
+        public static void CloseBrowser()
+        {
+            _seleniumBasicCheckBoxPage.CloseBrowser();
+        }
+
+    }
+}
---
+++ BasicCheckBoxDemoPage.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+
+
+//Nadodamiesi "page object pattern" struktūra, automatizuokite 3 testus:
+
+//https://www.seleniumeasy.com/test/basic-checkbox-demo.html
+//1. Pažymime “Click on this check box”, checpatikriname kad atsirado “Success - Check box is ked” pranešimas
+//2. Pažymime visas “Multiple Checkbox Demo” sekcijos varneles, patikriname kad mygtukas persivadino į “Uncheck All”
+//3. Paspaudžiame mygtuką “Uncheck All” , patikriname kad visos “Multiple Checkbox Demo” sekcijos varneles atžymėtos
+
+//Namų darbų pridavimo formą: atsiūskite nuorodą į jūsų GIT repositoriją(pasirūpinkite tuo kad ji būtų PUBLIC)
+
+
+namespace VcsWebdriver.Pages
+{
+    class BasicCheckBoxDemoPage : PageBase
+    {
+
+        private IWebElement PazymimeVarnele => Driver.FindElement(By.Id("isAgeSelected"));
+        private IWebElement AtsidaroPranesimas => Driver.FindElement(By.Id("txtAge"));
+
+        private IReadOnlyCollection<IWebElement> ElementuOptionSarasas => Driver.FindElements(By.ClassName("cb1-element"));
+        private IWebElement ShowMygtukas => Driver.FindElement(By.Id("check1"));
+
+
+
+
+        public BasicCheckBoxDemoPage(IWebDriver inputDriver) : base(inputDriver) //konstruktorius
+        {
+            Driver.Url = "https://www.seleniumeasy.com/test/basic-checkbox-demo.html";
+        }
+
+
+
+        public void ClickCheckBox()
+        {
+            PazymimeVarnele.Click();
+        }
+
+        public void SuccesTekstoRodymas(string expectedPranesimas)
+        {
+            Assert.AreEqual(expectedPranesimas, AtsidaroPranesimas.Text, $"Mes tikejomes teksto:{expectedPranesimas}, o gavome {AtsidaroPranesimas}");
+        }
+
+
+        //public void AssertTekstoRodymas(string expectedTekstas)
+        //{
+        //    string tekstas = AtsidaroPranesimas.Text;
+        //    Assert.AreEqual(expectedTekstas, tekstas, "Success - Check box is checked");
+        //}
+
+
+
+        public void ClickAllOptions()
+        {
+            foreach (IWebElement elementasOption in ElementuOptionSarasas)
+            {
+                elementasOption.Click();
+            }
+
+        }
+
+        public void MygtukoTekstoRezultatas(string expectedPranesimas2)
+        {
+            string MygtukoTekstas = ShowMygtukas.GetAttribute("value");
+            Assert.AreEqual(expectedPranesimas2, MygtukoTekstas, "Tekstas ne toks, kokio tikejomes.");
+        }
+
+
+
+
+        public void UncheckMygtukoPaspaudimas()
+        {
+            if (ShowMygtukas.GetAttribute("value") == "Check All")
+
+                ShowMygtukas.Click();
+
+            ShowMygtukas.Click();
+        }
+
+        public void UncheckMygtukoPatikra()
+        {
+            foreach (var option in ElementuOptionSarasas)
+                Assert.AreEqual(false, option.Selected, "Viena is varneliu pazymeta"); //option.Selected = duoda true, jei pazymeta varnele.
+
+        }
+    }
+}
